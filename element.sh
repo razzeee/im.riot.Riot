3712452a7f4e5cfba03477f4a99263fc06274466
Kolja Lampe <razzeee@gmail.com>
2022-03-22 22:01:34 +0100

    WIP

--- element.sh
+++ element.sh
@@ -7,4 +7,9 @@ then
     FLAGS="$FLAGS --disable-gpu-sandbox"
 fi
 
+if [[ $XDG_SESSION_TYPE == "wayland" ]]
+then
+    FLAGS="$FLAGS --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer,WaylandWindowDecorations --ozone-platform=wayland"
+fi
+
 env TMPDIR="$XDG_RUNTIME_DIR/app/${FLATPAK_ID:-im.riot.Riot}" zypak-wrapper /app/Element/element-desktop $FLAGS "$@"
